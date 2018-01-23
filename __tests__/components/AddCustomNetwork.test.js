0c8758d8c6aceb7c2ae4a19456758150478870f4
Silas Albrecht <silas.a.a@gmail.com>
2018-01-23 18:15:30 -0500

    Simplify test failure repro

--- __tests__/components/AddCustomNetwork.test.js
+++ __tests__/components/AddCustomNetwork.test.js
@@ -1,45 +1,13 @@
 import React from 'react'
-import configureStore from 'redux-mock-store'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import thunk from 'redux-thunk'
 import { mount } from 'enzyme'
 
 import AddCustomNetwork from '../../src/app/components/AddCustomNetwork/AddCustomNetwork'
 
-const setup = () => {
-  const store = configureStore([thunk])({})
-  const props = {
-    addCustomNetwork: jest.fn(),
-  }
-  const wrapper = mount(
-    <AddCustomNetwork />
-  )
-
-  return {
-    wrapper,
-  }
-}
-
 describe('AddCustomNetwork', () => {
-  test('renders without crashing', (done) => {
-    const { wrapper } = setup()
-    expect(wrapper).toMatchSnapshot()
-    done()
-  })
-
   test('adds network on form submit', () => {
-    const { wrapper } = setup()
-
-    const instance = wrapper.instance()
-
-    const networkNameField = wrapper.find('input')
-    networkNameField.simulate('change', { target: { value: 'Test Network' } })
-
-    const networkUrlField = wrapper.find('textfield').second().getElement()
-    inputFields[0].simulate('change', { target: { value: 'http://127.0.0.1:5000/' } })
-
-    wrapper.find('button').first().simulate('submit')
-    expect(instance.props.addCustomNetwork).toHaveBeenCalledWith(0)
+    const wrapper = mount(
+      <AddCustomNetwork />
+    )
+    expect(wrapper !== null).toEqual(true)
   })
 })
