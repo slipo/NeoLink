1a4b7afe666902ce702f59873fe26ba1c49c2f95
phetter <fet@fet>
2018-03-02 14:53:15 -0500

    variadic arguments

--- src/app/utils/neonWrappers.js
+++ src/app/utils/neonWrappers.js
@@ -15,7 +15,7 @@ export function callInvoke (networkUrl, account, input) {
       reject(new Error('Invalid asset type specified'))
     }
 
-    const txArgs = [input.arg1, input.arg2]
+    const txArgs = input.args
     const args = []
     txArgs.forEach((arg) => {
       if (arg) {
--- src/js/content.js
+++ src/js/content.js
@@ -14,18 +14,19 @@ window.addEventListener('message', (event) => {
     let operation = event.data.text.operation
     let assetType = event.data.text.assetType
     let assetAmount = event.data.text.assetAmount
-    let arg1 = event.data.text.arg1
-    let arg2 = event.data.text.arg2
+    // let arg1 = event.data.text.arg1
+    // let arg2 = event.data.text.arg2
+    let args = event.data.text.args
 
     // Send an invoke to the extension background page.
-    sendInvoke(scriptHash, operation, arg1, arg2, assetType, assetAmount)
+    sendInvoke(scriptHash, operation, args, assetType, assetAmount)
   }
 })
 
 // Send a message to background.js to run a smart contract send invoke
-function sendInvoke (scriptHash, operation, arg1, arg2, assetType, assetAmount) {
+// function sendInvoke (scriptHash, operation, arg1, arg2, assetType, assetAmount) {
+function sendInvoke (scriptHash, operation, args, assetType, assetAmount) {
   console.log('invoking contract from content script')
-  let args = [arg1, arg2]
 
   let tx = {
     'operation': operation,
